de15a984142ec06dcb016624b7122c57be286531
PawelRadawiec <pawel.radaw@gmail.com>
2022-05-19 18:28:57 +0200

    fork join component test, check statuses

--- src/app/modules/operators/components/fork-join/fork-join.component.spec.ts
+++ src/app/modules/operators/components/fork-join/fork-join.component.spec.ts
@@ -6,6 +6,7 @@ import {
   tick,
 } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { BlockStatus } from 'src/app/modules/shared/components/block/block.component';
 import { ForkJoinHelperService } from '../../services/fork-join-helper.service';
 
 import { ForkJoinComponent } from './fork-join.component';
@@ -41,8 +42,13 @@ fdescribe('ForkJoinComponent', () => {
       button.triggerEventHandler('click', null);
     });
     tick(8_000);
+    
+    const results = component.results;
+    expect(results.length).toBe(3);
 
-    expect(component.results.length).toBe(3);
+    expect(results[0].status).toBe(BlockStatus.EXECUTED);
+    expect(results[1].status).toBe(BlockStatus.EXECUTED);
+    expect(results[2].status).toBe(BlockStatus.EXECUTED);
   }));
 
   it('should not emit values', fakeAsync(() => {
