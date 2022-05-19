0e435bb4c14f1b57850ba4897563210c42e373cb
PawelRadawiec <pawel.radaw@gmail.com>
2022-05-19 12:34:48 +0200

    combine-latest - unit tests

--- src/app/modules/operators/components/combine-latest/combine-latest.component.spec.ts
+++ src/app/modules/operators/components/combine-latest/combine-latest.component.spec.ts
@@ -1,25 +1,81 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed,
+  tick,
+  waitForAsync,
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { BlockListComponent } from 'src/app/modules/shared/components/block-list/block-list.component';
+import { BlockComponent } from 'src/app/modules/shared/components/block/block.component';
 
 import { CombineLatestComponent } from './combine-latest.component';
 
-describe('CombineLatestComponent', () => {
+fdescribe('CombineLatestComponent', () => {
   let component: CombineLatestComponent;
   let fixture: ComponentFixture<CombineLatestComponent>;
+  let el: DebugElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CombineLatestComponent ]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        CombineLatestComponent,
+        BlockListComponent,
+        BlockComponent,
+      ],
     })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CombineLatestComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CombineLatestComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+      });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit three values', fakeAsync(() => {
+    fixture.detectChanges();
+
+    const buttons = el.queryAll(By.css('button'));
+    expect(buttons?.length).toBe(4);
+
+    buttons[0].triggerEventHandler('click', null);
+    buttons[1].triggerEventHandler('click', null);
+    buttons[2].triggerEventHandler('click', null);
+
+    tick(7_000);
+    fixture.detectChanges();
+
+    expect(component?.results?.length).toBe(3);
+    expect(component?.resultsHistory?.length).toBe(1);
+    expect(component?.resultsHistory?.[0]?.length).toBe(3);
+  }));
+
+  it('should show history', fakeAsync(() => {
+    fixture.detectChanges();
+    const buttons = el.queryAll(By.css('button'));
+    expect(buttons?.length).toBe(4);
+
+    buttons[0].triggerEventHandler('click', null);
+    buttons[1].triggerEventHandler('click', null);
+    buttons[2].triggerEventHandler('click', null);
+
+    tick(7_000);
+    fixture.detectChanges();
+    expect(component?.results?.length).toBe(3);
+
+    expect(component?.showHistory).toBe(false);
+
+    buttons[3].triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component?.showHistory).toBe(true);
+  }));
+
+
 });
